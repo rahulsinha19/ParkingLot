b2843eeec20ae8b307654496e908df8505dbb6ad
Charan <akisricharan12@gmail.com>
2022-07-31 16:37:11 +0530

    unpark feature added

--- src/test/java/bike/rapido/parkinglot/ParkingLotTest.java
+++ src/test/java/bike/rapido/parkinglot/ParkingLotTest.java
@@ -10,9 +10,9 @@ public class ParkingLotTest {
     @Test
     public void canDriverParkIfTotalSlotsAreMoreThan1() {
         int totalSlots = 10;
+
         new ParkingSpace(totalSlots);
         Driver driver = new Driver();
-
         boolean hasDriverParked = driver.checkAvailabilityAndPark();
 
         assertThat(hasDriverParked, is(true));
@@ -21,12 +21,36 @@ public class ParkingLotTest {
     @Test
     public void canSecondDriverParkIfTotalSlotsAre1() {
         int totalSlots = 1;
+
         new ParkingSpace(totalSlots);
-
         new Driver().checkAvailabilityAndPark();
-
         boolean hasDriverParked = new Driver().checkAvailabilityAndPark();
 
         assertThat(hasDriverParked, is(false));
+    }
+
+    @Test
+    public void canDriverUnParkCar() {
+        int totalSlots = 10;
+
+        new ParkingSpace(totalSlots);
+        new Driver().checkAvailabilityAndPark();
+        boolean hasDriverUnParked = new Driver().unPark();
+
+        assertThat(hasDriverUnParked, is(true));
+    }
+
+    @Test
+    public void canDriverUnParkFromEmptyParkingSpace() {
+        int totalSlots = 10;
+
+        new ParkingSpace(totalSlots);
+        Driver driver1 = new Driver();
+        driver1.checkAvailabilityAndPark();
+        driver1.unPark();
+        boolean hasAnotherDriverUnParked = new Driver().unPark();
+
+        assertThat(hasAnotherDriverUnParked, is(false));
+
     }
 }
